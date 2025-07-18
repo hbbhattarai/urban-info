9d7b9de27316cd5dd8a81fc632c151be16a847c0
hbbhattarai <hemchettri56.geo@gmail.com>
2025-07-18 20:57:02 +0600

    ADD

--- config/database.js
+++ config/database.js
@@ -1,16 +1,20 @@
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  logging: false,
+  // If using SSL (e.g., on Heroku), you might need this:
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = sequelize;
+
 
 module.exports = sequelize;
--- dbConfig/dbconfig.js
+++ dbConfig/dbconfig.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const connectionString = `${process.env.DB_URL}`;
 
 const pool = new Pool({
     connectionString: connectionString
