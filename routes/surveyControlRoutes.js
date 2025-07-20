1ae5b7c2391837b0c2deff5d9fc2f810e91e4b95
hbbhattarai <hemchettri56.geo@gmail.com>
2025-07-20 19:44:09 +0600

    ADD

--- routes/surveyControlRoutes.js
+++ routes/surveyControlRoutes.js
@@ -28,16 +28,7 @@ router.get('/get-data/:surveyId', surveyShapefileController.getData);
 router.get('/view/:surveyId', surveyShapefileController.renderSurveyData);
 
 // Multer setup for uploads folder
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../uploads'));
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
-  }
-});
+const storage = multer.memoryStorage()
 const upload = multer({ storage });
 
 // Parcel type selection page
