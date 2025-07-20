fea8f6e1a9297c594ffecc100faa5663579d13ac
hbbhattarai <hemchettri56.geo@gmail.com>
2025-07-20 11:02:14 +0600

    ADD

--- utils/googleDriveUploader.js
+++ utils/googleDriveUploader.js
@@ -1,22 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 const { google } = require('googleapis');
+const { Readable } = require('stream');
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, '../config/urban-info-survey-gmc.json'),
-  scopes: ['https://www.googleapis.com/auth/drive'],
-});
+let drive; // we'll initialize this once
 
-const drive = google.drive({ version: 'v3', auth });
+async function initializeDrive() {
+  if (drive) return drive; // reuse if already initialized
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: path.join(__dirname, '../config/urban-info-survey-gmc.json'),
+    scopes: ['https://www.googleapis.com/auth/drive'],
+  });
+
+  const authClient = await auth.getClient();
+  drive = google.drive({ version: 'v3', auth: authClient });
+  return drive;
+}
 
 async function uploadFileToDrive(file) {
+
+  const drive = await initializeDrive(); 
   const fileMetadata = {
     name: file.originalname,
   };
 
   const media = {
     mimeType: file.mimetype,
-    body: fs.createReadStream(file.path), // ✅ This must be a stream
+    body: Readable.from(file.buffer), // use memory buffer
   };
 
   const response = await drive.files.create({
